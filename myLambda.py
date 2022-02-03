27e4d164408b40ed4be53039cea5628c7b1b82c0
Mayank Koli <manukoli1986@gmail.com>
2022-02-04 00:20:33 +0530

    Update myLambda.py

--- myLambda.py
+++ myLambda.py
@@ -1,7 +1,24 @@
 import json
 import boto3
 
-# def checkExistingPolicy(bucketName):
+#Creating client connection with s3 service and create s3SSLEnable policy
+s3 = boto3.client('s3')
+
+def checkExistingPolicy(bucketName):
+    try:
+        response = s3.get_bucket_policy(
+            Bucket=bucketName,
+        )
+        if response is not None:
+            print("Bucket has policy enabled for bucket " + bucketName)
+    except s3.exceptions.from_code('NoSuchBucketPolicy') as e:
+        if e.response['Error']['Code'] == 'NoSuchBucketPolicy':
+            create_bucket_policy(bucketName)
+            print("Policy has been enabled for " + bucketName)
+            return 204
+
+
+
 def create_bucket_policy(bucketName):
     try:
         #Policy Content
@@ -28,17 +45,15 @@ def create_bucket_policy(bucketName):
     except Exception as e:
         print(e)
         
-    #Creating client connection with s3 service and create s3SSLEnable policy
-    s3 = boto3.client('s3')
+
     
     resp = s3.put_bucket_policy(Bucket=bucketName,Policy=policy_string)
     status = resp['ResponseMetadata']['HTTPStatusCode']
     return status
 
 
-
 def lambda_handler(event, context):
-    status = create_bucket_policy("qa-mayank-koli-platform-challenge")
+    status = checkExistingPolicy("qa-mayank-koli-platform-challenge")
     if status == 204:
         return {'body': json.dumps('Policy Implement')}
     else:
