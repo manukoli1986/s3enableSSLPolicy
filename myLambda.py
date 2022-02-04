ef6f6c0b00c9913684b5cda766d836cade4c4dee
Mayank Koli <manukoli1986@gmail.com>
2022-02-05 00:49:42 +0530

    Working lambda code

--- myLambda.py
+++ myLambda.py
@@ -10,15 +10,39 @@ def checkExistingPolicy(bucketName):
             Bucket=bucketName,
         )
         if response is not None:
-            print("Bucket has policy enabled for bucket " + bucketName)
+            print("{} bucket has already policy enabled so appending new policy in it.".format(bucketName))
+            
+            # #Appending s3SSL policy into exisitng policy
+            user_policy = {
+                        "Sid": "AllowSSSLRequestsOnly",
+                        "Effect": "Deny",
+                        "Principal": "*",
+                        "Action": "s3:*",
+                        "Resource": ["arn:aws:s3:::{}/*".format(bucketName), "arn:aws:s3:::{}".format(bucketName)],
+                        "Condition": {
+                            "Bool": {
+                                "aws:SecureTransport": "false"
+                            }
+                        }
+                    }
+            # Converting actual into dict
+            respToDict = json.loads(response['Policy'])
+            # print(type(respToDict))
+            
+            a_user_policy = user_policy.copy()
+            respToDict['Statement'].append(a_user_policy)
+            
+            #Converting dict to str as policy accept in string only
+            newPol = json.dumps(respToDict)
+            resp = s3.put_bucket_policy(Bucket=bucketName,Policy=newPol)
+            # print(resp)
+            return 500 
     except s3.exceptions.from_code('NoSuchBucketPolicy') as e:
         if e.response['Error']['Code'] == 'NoSuchBucketPolicy':
             create_bucket_policy(bucketName)
-            print("Policy has been enabled for " + bucketName)
+            # print("Policy has been enabled for " + bucketName)
             return 204
-
-
-
+    
 def create_bucket_policy(bucketName):
     try:
         #Policy Content
@@ -44,9 +68,6 @@ def create_bucket_policy(bucketName):
         
     except Exception as e:
         print(e)
-        
-
-    
     resp = s3.put_bucket_policy(Bucket=bucketName,Policy=policy_string)
     status = resp['ResponseMetadata']['HTTPStatusCode']
     return status
@@ -56,6 +77,8 @@ def lambda_handler(event, context):
     status = checkExistingPolicy("qa-mayank-koli-platform-challenge")
     if status == 204:
         return {'body': json.dumps('Policy Implement')}
+    elif status == 500:
+        return {'body': json.dumps('Appended in existing Policy')}
     else:
         return {'body': json.dumps('Error in Policy or code')}
     
